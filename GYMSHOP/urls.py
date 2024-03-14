de1bee83288af2d38372e84045696435412b0aa5
Gonzalez <alejogonzalez0341@gmail.com>
2024-03-13 19:26:33 -0500

    _

--- GYMSHOP/urls.py
+++ GYMSHOP/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from catalogo import views
 from home.views import home
+"""from .views import shop"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogo/', views.catalogo, name='catalogo'),
     path('', home)
 ]
+"""path("shop/", shop),"""
